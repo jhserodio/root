8bd4733dfa414833f93b9df09478ecbb9b965abb
jhserodio <jhserodio@gmail.com>
2017-03-30 23:19:50 -0300

    roboto mono

---
+++ src/app/components/RobotoMono.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// #CAF6FF
+
+const Wrapper = styled.div`
+  background: #efefef;
+  color: #121212;
+
+  h1 {
+    margin: 0;
+    font-family: 'Roboto Mono';
+    padding: 16px;
+    letter-spacing: .05em;
+    font-weight: normal;
+    font-size: 32px;
+    letter-space:
+  }
+`
+
+const RobotoMono = props => (
+  <Wrapper>
+    <h1>
+      Root
+    </h1>
+    <h1>
+      ROOT
+    </h1>
+    <h1>
+      root
+    </h1>
+  </Wrapper>
+)
+
+export default RobotoMono
