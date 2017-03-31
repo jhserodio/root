5a6aff3823e25b38885dfd23a9475274130a0561
jhserodio <jhserodio@gmail.com>
2017-03-30 23:19:35 -0300

    source sans

---
+++ src/app/components/Source.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background: #efefef;
+  color: #121212;
+
+  h1 {
+    margin: 0;
+    font-family: 'Source sans pro';
+    padding: 16px;
+    font-size: 32px;
+  }
+`
+
+const Source = props => (
+  <Wrapper>
+    <h1>
+      Root
+    </h1>
+    <h1>
+      ROOT
+    </h1>
+    <h1>
+      root
+    </h1>
+  </Wrapper>
+)
+
+export default Source
