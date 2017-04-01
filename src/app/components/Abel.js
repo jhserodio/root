97b43de6bb71822b22ac8d221729e6a5c50d571d
jhserodio <jhserodio@gmail.com>
2017-03-31 22:48:21 -0300

    abel

---
+++ src/app/components/Abel.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background: #efefef;
+  color: #121212;
+
+  h1 {
+    margin: 0;
+    font-family: 'Abel';
+    padding: 16px;
+    font-size: 32px;
+  }
+`
+
+const Abel = props => (
+  <Wrapper>
+    <h1>
+      root
+    </h1>
+  </Wrapper>
+)
+
+export default Abel
