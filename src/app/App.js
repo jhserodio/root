08339987d58d9348841989d0f94ea75ffd11bed0
jhserodio <jhserodio@gmail.com>
2017-04-03 19:58:39 -0300

    include mock

--- src/app/App.js
+++ src/app/App.js
@@ -1,51 +1,16 @@
 import React, { Component } from 'react'
 
+import { mock } from './mock'
+
 import Header from './components/Header'
 import Grid from './components/Grid'
-import Noto from './components/Noto'
-import Valera from './components/Valera'
-import Roboto from './components/Roboto'
-import RobotoMono from './components/RobotoMono'
-import Quicksand from './components/Quicksand'
-import Itim from './components/Itim'
-import Oswald from './components/Oswald'
-import Raleway from './components/Raleway'
-import Source from './components/Source'
-import MontSerrat from './components/MontSerrat'
-import Lato from './components/Lato'
-import Josefin from './components/Josefin'
-import Dosis from './components/Dosis'
-import Abel from './components/Abel'
 
 class App extends Component {
 
   constructor() {
     super();
 
-    this.state = {
-      colors: {
-        smooth: '#DEFDF0',
-        brand: '#152A38',
-        other: '#E71D36'
-      },
-      brands: ['root', 'Lich', 'ESTON'],
-      fonts: [
-        'Roboto',
-        'Roboto Mono',
-        'Quicksand',
-        'Itim',
-        'Oswald',
-        'Raleway',
-        'Source sans pro',
-        'MontSerrat',
-        'Lato',
-        'Josefin',
-        'Dosis',
-        'Abel',
-        'Valera',
-        'Noto sans'
-      ]
-    }
+    this.state = mock.load();
   }
 
   render() {
@@ -57,20 +22,7 @@ class App extends Component {
         <Header />
         <main>
           <Grid>
-            <Noto brands={brands}/>
-            <Valera brands={brands}/>
-            <RobotoMono brands={brands}/>
-            <Roboto brands={brands}/>
-            <Quicksand brands={brands}/>
-            <Itim brands={brands}/>
-            <Oswald brands={brands}/>
-            <Raleway brands={brands}/>
-            <Source brands={brands}/>
-            <MontSerrat brands={brands}/>
-            <Lato brands={brands}/>
-            <Josefin brands={brands}/>
-            <Dosis brands={brands}/>
-            <Abel brands={brands}/>
+            
           </Grid>
         </main>
       </div>
---
+++ src/app/mock.js
@@ -0,0 +1,28 @@
+export const mock = {
+  load() {
+    return {
+      colors: {
+        smooth: '#DEFDF0',
+        brand: '#152A38',
+        other: '#E71D36'
+      },
+      brands: ['root', 'Lich', 'ESTON'],
+      fonts: [
+        'Roboto',
+        'Roboto Mono',
+        'Quicksand',
+        'Itim',
+        'Oswald',
+        'Raleway',
+        'Source sans pro',
+        'MontSerrat',
+        'Lato',
+        'Josefin',
+        'Dosis',
+        'Abel',
+        'Valera',
+        'Noto sans'
+      ]
+    }
+  }
+}
